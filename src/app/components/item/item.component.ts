e37c19e5d8a22d639019245861375e93d9251f3d
Vitali <vitgen1997@mail.ru>
2018-11-19 22:01:37 +0300

    add inputs

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-item',
@@ -6,6 +6,7 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./item.component.scss']
 })
 export class ItemComponent implements OnInit {
+  @ViewChild('')
   @Input() name: string;
   @Input() index: number;
   @Output() onChangeNumber: EventEmitter<Number> = new EventEmitter();
@@ -16,8 +17,11 @@ export class ItemComponent implements OnInit {
   ngOnInit() {
   }
 
-  changeValue(number) {
-    this.onChangeNumber.emit(number);
+  changeValue(valueNumber) {
+    
+    console.log(valueNumber);
+    const value = (<HTMLInputElement>valueNumber).value.trim();
+    this.onChangeNumber.emit(Number(value));
   }
 
 }
